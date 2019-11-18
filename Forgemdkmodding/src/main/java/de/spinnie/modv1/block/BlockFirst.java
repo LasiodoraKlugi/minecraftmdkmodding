f541f152ef6170480129e69056521c19ec9844d3
LasiodoraKlugi <tobiassdischinger@t-online.de>
2019-11-18 16:57:20 +0100

    added Block Eigenschaften:
    Hardness
    Resistance
    SoundType
    Harvest Level(pickaxe, iron)
    LightLevel
    Drop random(min 1, max 20)
    Drop moditem

--- Forgemdkmodding/src/main/java/de/spinnie/modv1/block/BlockFirst.java
+++ Forgemdkmodding/src/main/java/de/spinnie/modv1/block/BlockFirst.java
@@ -1,15 +1,34 @@
 package de.spinnie.modv1.block;
 
+import java.util.Random;
+
+import de.spinnie.modv1.init.ModItems;
 import de.spinnie.modv1.init.modv1Tabs;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
 
 public class BlockFirst extends Block {
 
 	public BlockFirst() {
 		super(Material.ROCK);
 		setCreativeTab(modv1Tabs.tab);
-
+		setHardness(4.5F);
+		setResistance(90F);
+		setSoundType(SoundType.SNOW);
+		setHarvestLevel("pickaxe", 2);  //Harvest level nachsehen mit F3 drücken und klicken auf Harvest
+		setLightLevel(0.8F);            // strg+shift+h open search menue and search for ItemTool -> List with items(pickusw)
 	}
 
+	@Override
+	public int quantityDropped(IBlockState state, int fortune, Random random) {
+		return random.nextInt(20) +1;             //dropt min 1 bis 20 items random
+	}
+	
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return ModItems.moditem;       //dropt mein Item beim abbau                     
+	}
 }
